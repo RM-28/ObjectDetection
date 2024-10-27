911f97cc4215f5004c4d7bd9861d3d7ed7325647
LennyKiang <kenny712126@gmail.com>
2024-10-26 22:24:22 -0400

    update 2

--- predict.py
+++ predict.py
@@ -1,9 +1,16 @@
 from ultralytics import YOLO
+from PIL import Image
 
 if __name__ == '__main__':
-    model = YOLO("predictModel.pt")
+    model = YOLO("best.pt")
 
-    model.predict(source = "sofa2.png",show = True, save = True, conf = 0.5, line_thickness = 2)
+    results = model.tune(data="data.yaml", epochs=100, workers = 4, iterations = 100, optimizer="AdamW", plots=False, save=False, val=False)
+    # path = Image.open("888.png")
+    
+    # newImage = path.resize((640, 640))
+    # newImage.save("finalImage.png")
+    
+    # model.predict(source = "finalImage.png",show = True, save = True, conf = 0.5, line_thickness = 2)
     
     # metrics = model.val()
     # print(metrics.box.map)
--- training.py
+++ training.py
@@ -4,9 +4,17 @@ from matplotlib import pyplot as plt
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO("yolo11n.pt")
+    # lets keep 11s for now.
+    # 11m is a lot slower.
+    model = YOLO("yolo11s.pt")
+    
+    # scale keyword migth improve detection quality for far/near objects.
+    # cache keyword is caching images in disk
+    # amp keyword is for mixed precision training
+    # patience keyword is for early stopping, the model will stop training if the validation loss does not improve for 10 epochs.
+    # adam optimizer: free model tuning (probably good)
+    train_results = model.train(data = "data.yaml", epochs = 100, imgsz = 640, device = "cuda", batch = 24, cache = 'disk', amp = True, patience = 10, optimizer = 'auto')
 
-    train_results = model.train(data = "data.yaml", epochs = 100, imgsz = 640, device = "cuda", batch = 32)
-
-    metrics = model.val()
-
+    # model.tune(data="data.yaml", epochs=30, iterations = 100, optimizer="AdamW", plots=False, save=False, val=False)
+    # metrics = model.val()
+    # print(metrics.box.map)
